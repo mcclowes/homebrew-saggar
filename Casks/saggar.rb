a8eb26c48d3c702f65949073a72ad8b4cb627f62
Max Clayton Clowes <contact@mcclowes.com>
2026-08-08 08:24:29 +0100

    Update Saggar to 0.14.0

--- Casks/saggar.rb
+++ Casks/saggar.rb
@@ -1,6 +1,6 @@
 cask "saggar" do
-  version "0.13.0"
-  sha256 "a97ce2670a6b6cda41d6ae07c256407a619abc46c58f32448685cf04bc6c61ce"
+  version "0.14.0"
+  sha256 "910faaee0b24163ec8b90f1bbc390355d659a55e504e6e598e4c5d94da71332e"
 
   url "https://github.com/mcclowes/homebrew-saggar/releases/download/v#{version}/Saggar-#{version}.zip"
   name "Saggar"
