d31fa06a98da7796f6de3799a17688d999f7f187
Max Clayton Clowes <max@mcclowes.com>
2026-08-16 12:34:47 +0100

    saggar 0.23.2

--- Casks/saggar.rb
+++ Casks/saggar.rb
@@ -1,6 +1,6 @@
 cask "saggar" do
-  version "0.23.1"
-  sha256 "1899f30dd44f8f664b78b3dfa1fabd254ed5a56e5542d7481bf24d2028017265"
+  version "0.23.2"
+  sha256 "3a5881b5ff4009ceea2ae0c1a957b5f2994cf7814016e9d798a54fd8dd80c744"
 
   url "https://github.com/mcclowes/homebrew-saggar/releases/download/v#{version}/Saggar-#{version}.zip"
   name "Saggar"
@@ -8,7 +8,7 @@ cask "saggar" do
   homepage "https://github.com/mcclowes/saggar"
 
   depends_on macos: :tahoe
-  depends_on arch: :arm64
+  depend_on arch: :arm64
 
   app "Saggar.app"
 
